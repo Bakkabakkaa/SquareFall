53c13fb281ef101a07215f2f7bf1db9b842d41f0
Art'm <kasko.artem45@gmail.com>
2025-12-08 23:42:54 +0300

    player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,36 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Transform _leftBoarder;
+    [SerializeField] private Transform _rightBoarder;
+    [SerializeField] private float _speed;
+    private bool _isMovingRight;
+    private float _oneWayTime;
+    private float _currentTime;
+
+    private void Awake()
+    {
+        _oneWayTime = Vector3.Distance(_leftBoarder.position, _rightBoarder.position) / _speed;
+        _currentTime = Vector3.Distance(_leftBoarder.position, transform.position) / _speed;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
     public void ChangeDirection()
     {
-        Debug.Log("Direction has been changed");
+        _isMovingRight = !_isMovingRight;
+    }
+
+    private void Move()
+    {
+        _currentTime += _isMovingRight ? Time.deltaTime : -Time.deltaTime;
+
+        var progress = Mathf.PingPong(_currentTime, _oneWayTime) / _oneWayTime;
+        transform.position = Vector3.Lerp(_leftBoarder.position, _rightBoarder.position, progress);
     }
 }
