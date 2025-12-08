b08aa5df2a40b75b58ac667b5ef9aeaae31ec1d0
Art'm <kasko.artem45@gmail.com>
2025-12-09 01:48:36 +0300

    cube counter

---
+++ Assets/Scripts/UI/ScoreController.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _currentScoreLabel;
+    [SerializeField] private int _scorePerSquare;
+
+    private int _currentScore;
+
+    public void AddScore()
+    {
+        _currentScore += _scorePerSquare;
+
+        _currentScoreLabel.text = _currentScore.ToString();
+    }
+}
