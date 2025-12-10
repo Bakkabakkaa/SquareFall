840fbc2858f685383993befe15b4edc0c2eb832e
Art'm <kasko.artem45@gmail.com>
2025-12-11 02:18:36 +0300

    saving the best result

--- Assets/Scripts/UI/ScoreController.cs
+++ Assets/Scripts/UI/ScoreController.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 
@@ -5,18 +6,39 @@ public class ScoreController : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _currentScoreLabel;
     [SerializeField] private int _scorePerSquare;
+    [SerializeField] private AudioSource _bestScoreSound;
 
+    private const string BEST_SCORE = "BestScore";
+
+    private int _bestScore;
     private int _currentScore;
+
+    private void Awake()
+    {
+        _bestScore = PlayerPrefs.GetInt(BEST_SCORE);
+    }
 
     public void AddScore()
     {
         _currentScore += _scorePerSquare;
-
         _currentScoreLabel.text = _currentScore.ToString();
     }
 
     public int GetCurrentScore()
     {
         return _currentScore;
+    }
+
+    public int GetBestScore()
+    {
+        if (_currentScore > _bestScore)
+        {
+            _bestScore = _currentScore;
+            PlayerPrefs.SetInt(BEST_SCORE, _bestScore);
+            PlayerPrefs.Save();
+            _bestScoreSound.Play();
+        }
+
+        return _bestScore;
     }
 }
